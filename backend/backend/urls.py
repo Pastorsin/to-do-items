d42bd307b01e242efb13f818204cd69a9ea08ad7
Andrés Milla <andressmilla@gmail.com>
2021-11-18 00:53:01 -0300

    Add token authentication to the API

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from doc.views import redoc_view
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path("", redoc_view),
     path("docs/", include("doc.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("to_do.urls")),
+    path("auth/", views.obtain_auth_token),
 ]
